ec23fd5b1aaadd271e78a74f234b5659d47128d0
BDNelson123 <ben.david.nelson@gmail.com>
2015-03-05 19:53:38 +0000

    added clubs and levels to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
   namespace :v1 do
+    resources :clubs
     resources :courses
+    resources :levels
   end
 end
