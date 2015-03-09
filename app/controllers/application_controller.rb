e8ac9085de0c49d32ddbaebfc645e8236491da11
BDNelson123 <ben.david.nelson@gmail.com>
2015-03-09 14:52:40 +0000

    took out extra whitespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
-
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   respond_to :json
 
   protected
