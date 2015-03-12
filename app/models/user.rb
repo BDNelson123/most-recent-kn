0b9573714de1790f11546b9c917822793c139247
BDNelson123 <ben.david.nelson@gmail.com>
2015-03-12 17:44:45 +0000

    locked down validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,41 +22,19 @@ class User < ActiveRecord::Base
   validates :dob, :presence => true
   validates :owns_clubs, :inclusion => {:in => [true, false], :message => "must be yes or no"}
   validates :iron_club_id, :presence => true
-  #validates_model_id :iron_club_id, :message => "must be a valid club brand", :model => Club
-  validate :iron_wood_id
+  validates_model_id :iron_club_id, :message => "must be a valid club brand", :model => Club
   validates :wood_club_id, :presence => true
-  validate :include_wood_id
+  validates_model_id :wood_club_id, :message => "must be a valid club brand", :model => Club
   validates :level_id, :presence => true
-  validate :include_level_id
-  # handedness has a value of 0 for left, and 1 for right
-  validates :handedness, :inclusion => {:in => [true, false], :message => "must be left or right"}
-  # gender has a value of 0 for female, and 1 for male
-  validates :gender, :inclusion => {:in => [true, false], :message => "must be male or female"}
+  validates_model_id :level_id, :message => "must be a valid level option", :model => Level
+  validates :handedness, :inclusion => {:in => [true, false], :message => "must be left or right"} # 0 for left, and 1 for right
+  validates :gender, :inclusion => {:in => [true, false], :message => "must be male or female"} # 0 for female, and 1 for male
   validates :email, :presence => true
   validates_email_format_of :email
   validates :email_optin, :inclusion => {:in => [true, false], :message => "must be yes or no"}
   validates :terms_accepted, :inclusion => {:in => [true, false], :message => "must be yes or no"}
-  validate :include_income_id
+  validates :income_id, :presence => true
+  validates_model_id :income_id, :message => "must be a valid income option", :model => Income
 
   scope :common_attributes, -> { select('name, nickname, image, email, address, address2, city, state, zip, phone, dob, handedness, owns_clubs, email_optin, terms_accepted, gender')}
-
-  # custom validation for wood_club_id
-  def include_wood_id
-    errors.add(:wood_club_id, "must be a valid brand") unless Common.model_array(Club).include?(wood_club_id)
-  end
-
-  # custom validation for iron_club_id
-  def include_iron_id
-    errors.add(:iron_club_id, "must be a valid brand") unless Common.model_array(Club).include?(iron_club_id)
-  end
-
-  # custom validation for level_id
-  def include_level_id
-    errors.add(:level_id, "must be a valid level") unless Common.model_array(Level).include?(level_id)
-  end
-
-  # custom validation for level_id
-  def include_income_id
-    errors.add(:income_id, "must be a valid income") unless Common.model_array(Income).include?(income_id)
-  end
 end
