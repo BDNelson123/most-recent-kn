5e2839a0f9b0b3312a555b1744fd9c1cd3dff122
BDNelson123 <ben.david.nelson@gmail.com>
2015-03-12 17:43:56 +0000

    added fields to auth sanitize for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,13 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:name, :address, :address2, :city, :state, :zip, :phone, :handedness, :dob, :owns_clubs, :gender, :wood_club_id, :iron_club_id]
+    devise_parameter_sanitizer.for(:sign_up) << [
+      :name, :address, :address2, 
+      :city, :state, :zip, 
+      :phone, :handedness, :dob, 
+      :owns_clubs, :gender, :wood_club_id, 
+      :iron_club_id, :level_id, :income_id, 
+      :email_optin, :terms_accepted
+    ]
   end
 end
