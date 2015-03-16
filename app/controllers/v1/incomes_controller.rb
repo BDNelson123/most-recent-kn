df628a2c0a83ded7c700c4dc90306ff69421e507
BDNelson123 <ben.david.nelson@gmail.com>
2015-03-16 17:53:23 +0000

    fixed show action for if record not there and ordered show action by id

--- app/controllers/v1/incomes_controller.rb
+++ app/controllers/v1/incomes_controller.rb
@@ -2,10 +2,16 @@ class V1::IncomesController < ApplicationController
   respond_to :json
 
   def index
-    render :json => Income.common_attributes.all
+    render :json => Income.common_attributes.all.order(:id => "ASC")
   end
 
   def show
-    render :json => Income.common_attributes.find_by_id(params[:id])
+    income = Income.common_attributes.find_by_id(params[:id])
+
+    if income
+      render :json => income
+    else
+      render :json => { :errors => "The income with id #{params[:id]} could not be found." }, :status => 422
+    end
   end
 end
