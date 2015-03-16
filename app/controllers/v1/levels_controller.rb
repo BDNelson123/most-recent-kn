32af35fafbbeb4ff2d9a097b92100b69f98aaa44
BDNelson123 <ben.david.nelson@gmail.com>
2015-03-16 18:45:14 +0000

    index and show actions for levels

--- app/controllers/v1/levels_controller.rb
+++ app/controllers/v1/levels_controller.rb
@@ -1,2 +1,17 @@
 class V1::LevelsController < ApplicationController
+  respond_to :json
+
+  def index
+    render :json => Level.common_attributes.all.order(:id => "DESC")
+  end
+
+  def show
+    level = Level.common_attributes.find_by_id(params[:id])
+
+    if level
+      render :json => level
+    else
+      render :json => { :errors => "The level with id #{params[:id]} could not be found." }, :status => 422
+    end
+  end
 end
