0d20840f774ad8d6ffa840f55c5972ad2a388cf3
BDNelson123 <ben.david.nelson@gmail.com>
2015-03-16 16:41:56 +0000

    specs for v1 clubs controller and fixed show action on clubs controllerg

--- app/controllers/v1/clubs_controller.rb
+++ app/controllers/v1/clubs_controller.rb
@@ -2,10 +2,16 @@ class V1::ClubsController < ApplicationController
   respond_to :json
 
   def index
-    render :json => Club.common_attributes.all
+    render :json => Club.common_attributes.all.order(:name => "ASC")
   end
 
   def show
-    render :json => Club.common_attributes.find_by_id(params[:id])
+    club = Club.common_attributes.find_by_id(params[:id])
+
+    if club
+      render :json => club
+    else
+      render :json => { :errors => "The club with id #{params[:id]} could not be found." }, :status => 422
+    end
   end
 end
--- spec/controllers/v1/clubs_controller_spec.rb
+++ spec/controllers/v1/clubs_controller_spec.rb
@@ -1,5 +1,64 @@
 require 'spec_helper'
 
-describe V1::ClubsController do
+describe V1::ClubsController, :type => :api do
+  before(:all) do
+    @club1 = FactoryGirl.create(:club, :name => "Callaway Golf")
+    @club2 = FactoryGirl.create(:club, :name => "Ben Hogan")
+    @club3 = FactoryGirl.create(:club, :name => "Bridgestone")
+  end
 
+  after(:all) do
+    Club.destroy_all
+  end
+
+  # INDEX action tests
+  describe "#index" do
+    it "should return a response of 200" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+
+    it "should return 3 total rows" do
+      get :index
+      expect(JSON.parse(response.body).length).to eq(3)
+    end
+
+    # NOTE: index controller orders by name desc
+    it "should return the correct values" do
+      get :index
+      expect(JSON.parse(response.body)[0]['name'].to_s).to eq(@club2.name.to_s)
+      expect(JSON.parse(response.body)[1]['name'].to_s).to eq(@club3.name.to_s)
+      expect(JSON.parse(response.body)[2]['name'].to_s).to eq(@club1.name.to_s)
+    end
+  end
+
+  # SHOW action tests
+  describe "#show" do
+    it "should return a response of 200" do
+      get :show, { 'id' => @club1.id }
+      expect(response.status).to eq(200)
+    end
+
+    it "should return 1 record" do
+      get :show, { 'id' => @club1.id }
+      expect(JSON.parse(response.body).length).to eq(2) # 2 fields for one record (id and name)
+    end
+
+    it "should return the correct data for club1" do
+      get :show, { 'id' => @club1.id }
+      expect(JSON.parse(response.body)['id'].to_i).to eq(@club1.id.to_i)
+      expect(JSON.parse(response.body)['name'].to_s).to eq(@club1.name.to_s)
+    end
+
+    it "should return the correct data for club2" do
+      get :show, { 'id' => @club2.id }
+      expect(JSON.parse(response.body)['id'].to_i).to eq(@club2.id.to_i)
+      expect(JSON.parse(response.body)['name'].to_s).to eq(@club2.name.to_s)
+    end
+
+    it "should return the correct error if the club id can't be found" do
+      get :show, { 'id' => @club3.id + 1 }
+      expect(JSON.parse(response.body)['errors'].to_s).to eq("The club with id #{@club3.id + 1} could not be found.")
+    end
+  end
 end
