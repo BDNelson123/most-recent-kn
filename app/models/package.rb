d4ca426e6f7c7de35f4ae3ed2aa0c6094812b80e
BDNelson123 <ben.david.nelson@gmail.com>
2015-03-24 11:46:50 -0400

    added credits to package attributes returned

--- app/models/package.rb
+++ app/models/package.rb
@@ -9,7 +9,7 @@ class Package < ActiveRecord::Base
   validates :price, :presence => true, :numericality => { :only_intger => true }
   validates :credits, :presence => true, :numericality => { :only_intger => true }
   
-  scope :common_attributes, -> {select('id, name, description, price')}
-  scope :feature_attributes, -> {select('packages.id,packages.name,packages.description,packages.price,group_concat(features.name) as package_features')}
+  scope :common_attributes, -> {select('id, name, description, price, credits')}
+  scope :feature_attributes, -> {select('packages.id,packages.name,packages.description,packages.price,packages.credits,group_concat(features.name) as package_features')}
   scope :feature_join, -> {joins('LEFT JOIN featurizations ON featurizations.package_id = packages.id LEFT JOIN features ON features.id = featurizations.feature_id')}
 end
