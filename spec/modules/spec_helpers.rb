3373ef1ae6e61b948e37584a262e4d8f225571a5
BDNelson123 <ben.david.nelson@gmail.com>
2015-03-26 18:04:08 -0400

    factory delete alphabetical order & factorygirl for club name changed to wilson

--- spec/modules/spec_helpers.rb
+++ spec/modules/spec_helpers.rb
@@ -1,19 +1,19 @@
 module SpecHelpers
   def delete_factories
-    Package.destroy_all
+    Club.destroy_all
+    Course.destroy_all
     Feature.destroy_all
     Featurization.destroy_all
-    User.destroy_all
-    Club.destroy_all
-    Level.destroy_all
     Income.destroy_all
-    Course.destroy_all
+    Level.destroy_all
+    Package.destroy_all
+    User.destroy_all
   end
 
   def create_user
     @income = FactoryGirl.create(:income)
     @level = FactoryGirl.create(:level)
-    @club = FactoryGirl.create(:club)
+    @club = FactoryGirl.create(:club, :name => "Wilson")
     @user = FactoryGirl.create(:user, :wood_club_id => @club.id, :iron_club_id => @club.id, :level_id => @level.id, :income_id => @income.id)
   end
 end
