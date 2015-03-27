29fb5def3ccd4c462ef389ad0ccc1b81f41a8558
BDNelson123 <ben.david.nelson@gmail.com>
2015-03-27 13:09:33 -0400

    more specs for levels and added create action

--- app/controllers/v1/levels_controller.rb
+++ app/controllers/v1/levels_controller.rb
@@ -1,5 +1,16 @@
 class V1::LevelsController < ApplicationController
   respond_to :json
+  before_action :authenticate_user!, :only => [:create, :update, :destroy], :unless => :master_api_key?
+
+  def create
+    level = Level.new(level_params)
+
+    if level.save
+      render :json => { :data => Level.common_attributes.find_by_id(level.id) }, :status => 201
+    else
+      render :json => { :errors => level.errors.full_messages.to_sentence }, :status => 422
+    end
+  end
 
   def index
     render :json => { :data => Level.common_attributes.all.order(:id => "DESC") }, :status => 200
@@ -13,5 +24,13 @@ class V1::LevelsController < ApplicationController
     else
       render :json => { :errors => "The level with id #{params[:id]} could not be found." }, :status => 422
     end
+  end
+
+  private
+
+  def level_params
+    _params = params.require(:level).permit(
+      :name, :description, :handicap
+    )
   end
 end
--- spec/controllers/v1/levels_controller_spec.rb
+++ spec/controllers/v1/levels_controller_spec.rb
@@ -1,20 +1,192 @@
 require 'spec_helper'
+include Devise::TestHelpers
 include SpecHelpers
 
 describe V1::LevelsController do
   before(:all) do
     delete_factories
+    create_user # move create_user to before level factories to make index tests work
 
-    @level1 = FactoryGirl.create(:level, :name => "Touring Pro", :handicap => "0-5 Handicap", :description => "Scratch Golfer")
-    @level2 = FactoryGirl.create(:level, :name => "Varsity", :handicap => "6-10 Handicap", :description => "Plays frequently")
-    @level3 = FactoryGirl.create(:level, :name => "Junior Varsity", :handicap => "11-20 Handicap", :description => "Plays frequently during the season")
-    @level4 = FactoryGirl.create(:level, :name => "Casual", :handicap => "20+ Handicap", :description => "Plays a couple of times per year")
-    @level5 = FactoryGirl.create(:level, :name => "Junior", :handicap => "No Handicap", :description => "Rarely Plays")
+    @level1 = FactoryGirl.create(:level)
+    @level2 = FactoryGirl.create(:level)
+    @level3 = FactoryGirl.create(:level)
+    @level4 = FactoryGirl.create(:level)
+    @level5 = FactoryGirl.create(:level)
   end
 
   after(:all) do
     delete_factories
   end
+
+  # --------------------------------------------- #
+  # --------------------------------------------- #
+  # --------------------------------------------- #
+
+  # CREATE action tests
+  describe "#create" do
+    context "authentication" do
+      it "should return a response status of 401 if user is not logged in" do
+        post :create, format: :json, :level => {:name => "test", :description => "test", :handicap => "test"}
+        expect(response.status).to eq(401)
+      end
+
+      # --------------- #
+
+      it "should return a response status of 201 if user is logged in" do
+        sign_in @user
+        request.headers.merge!(@user.create_new_auth_token)
+        post :create, format: :json, :level => {:name => "test", :description => "test", :handicap => "test"}
+        expect(response.status).to eq(201)
+      end
+    end
+
+    # ------------------------------ #
+    # ------------------------------ #
+
+    context "response status" do
+      context "validations" do
+        it "should return a response status of 422 if record is not created - name not present" do
+          sign_in @user
+          request.headers.merge!(@user.create_new_auth_token)
+          post :create, format: :json, :level => {:name => nil, :description => "test", :handicap => "test"}
+          expect(response.status).to eq(422)
+        end
+
+        # --------------- #
+
+        it "should return a response status of 422 if record is not created - description not present" do
+          sign_in @user
+          request.headers.merge!(@user.create_new_auth_token)
+          post :create, format: :json, :level => {:name => "test", :description => nil, :handicap => "test"}
+          expect(response.status).to eq(422)
+        end
+
+        # --------------- #
+
+        it "should return a response status of 422 if record is not created - handicap not present" do
+          sign_in @user
+          request.headers.merge!(@user.create_new_auth_token)
+          post :create, format: :json, :level => {:name => "test", :description => "test", :handicap => nil}
+          expect(response.status).to eq(422)
+        end
+
+        # --------------- #
+
+        it "should return a response status of 422 if record already exists - validation for unique name" do
+          sign_in @user
+          request.headers.merge!(@user.create_new_auth_token)
+
+          level6 = FactoryGirl.create(:level, :name => "test")
+          post :create, format: :json, :level => {:name => "test", :description => "test", :handicap => "test"}
+          expect(response.status).to eq(422)
+          level6.destroy
+        end
+      end
+    end
+
+    # ------------------------------ #
+    # ------------------------------ #
+
+    context "level record creation" do
+      it "should add new record to db" do
+        sign_in @user
+        request.headers.merge!(@user.create_new_auth_token)
+        expect {post :create, format: :json, :level => {:name => "test", :description => "test", :handicap => "test"}}.to change(Level, :count).by(1)
+      end
+
+      # --------------- #
+
+      it "should not add new record to db - not signed in" do
+        expect {post :create, format: :json, :level => {:name => "test", :description => "test", :handicap => "test"}}.to change(Level, :count).by(0)
+      end
+
+      # --------------- #
+
+      it "should not add new record to db - validation fail for name - nil" do
+        sign_in @user
+        request.headers.merge!(@user.create_new_auth_token)
+        expect {post :create, format: :json, :level => {:name => nil, :description => "test", :handicap => "test"}}.to change(Level, :count).by(0)
+      end
+
+      # --------------- #
+
+      it "should not add new record to db - validation fail for handicap - nil" do
+        sign_in @user
+        request.headers.merge!(@user.create_new_auth_token)
+        expect {post :create, format: :json, :level => {:name => "test", :description => "test", :handicap => nil}}.to change(Level, :count).by(0)
+      end
+
+      # --------------- #
+
+      it "should not add new record to db - validation fail for description - nil" do
+        sign_in @user
+        request.headers.merge!(@user.create_new_auth_token)
+        expect {post :create, format: :json, :level => {:name => "test", :description => nil, :handicap => "test"}}.to change(Level, :count).by(0)
+      end
+
+      # --------------- #
+
+      it "should not add new record to db - validation fail for name - not unique" do
+        sign_in @user
+        request.headers.merge!(@user.create_new_auth_token)
+        expect {post :create, format: :json, :level => {:name => @level1.name.to_s, :description => "test", :handicap => "test"}}.to change(Level, :count).by(0)
+      end
+    end
+
+    # ------------------------------ #
+    # ------------------------------ #
+
+    context "returned data" do
+      it "should return the object id and name in the data hash" do
+        sign_in @user
+        request.headers.merge!(@user.create_new_auth_token)
+        post :create, format: :json, :level => {:name => "test name", :description => "test description", :handicap => "test handicap"}
+        expect(JSON.parse(response.body)['data'].to_s).to include("test name")
+        expect(JSON.parse(response.body)['data'].to_s).to include("test description")
+        expect(JSON.parse(response.body)['data'].to_s).to include("test handicap")
+      end
+
+      # --------------- #
+
+      it "should return the validation error for name already being taken" do
+        sign_in @user
+        request.headers.merge!(@user.create_new_auth_token)
+        post :create, format: :json, :level => {:name => @level1.name.to_s, :description => "test description", :handicap => "test handicap"}
+        expect(JSON.parse(response.body)['errors'].to_s).to include("Name has already been taken")
+      end
+
+      # --------------- #
+
+      it "should return the validation error for name being null" do
+        sign_in @user
+        request.headers.merge!(@user.create_new_auth_token)
+        post :create, format: :json, :level => {:name => nil, :description => "test description", :handicap => "test handicap"}
+        expect(JSON.parse(response.body)['errors'].to_s).to include("Name can't be blank")
+      end
+
+      # --------------- #
+
+      it "should return the validation error for description being null" do
+        sign_in @user
+        request.headers.merge!(@user.create_new_auth_token)
+        post :create, format: :json, :level => {:name => "test name", :description => nil, :handicap => "test handicap"}
+        expect(JSON.parse(response.body)['errors'].to_s).to include("Description can't be blank")
+      end
+
+      # --------------- #
+
+      it "should return the validation error for handicap being null" do
+        sign_in @user
+        request.headers.merge!(@user.create_new_auth_token)
+        post :create, format: :json, :level => {:name => "test name", :description => "test description", :handicap => nil}
+        expect(JSON.parse(response.body)['errors'].to_s).to include("Handicap can't be blank")
+      end
+    end
+  end
+
+  # --------------------------------------------- #
+  # --------------------------------------------- #
+  # --------------------------------------------- #
 
   # INDEX action tests
   describe "#index" do
@@ -23,10 +195,15 @@ describe V1::LevelsController do
       expect(response.status).to eq(200)
     end
 
+    # --------------- #
+
     it "should return 5 total rows" do
+      #NOTE: 6 comes from user model needing to create a level
       get :index
-      expect(JSON.parse(response.body)['data'].length).to eq(5)
+      expect(JSON.parse(response.body)['data'].length).to eq(6)
     end
+
+    # --------------- #
 
     # NOTE: index controller orders by id desc
     it "should return the correct values" do
@@ -53,6 +230,10 @@ describe V1::LevelsController do
     end
   end
 
+  # --------------------------------------------- #
+  # --------------------------------------------- #
+  # --------------------------------------------- #
+
   # SHOW action tests
   describe "#show" do
     it "should return a response of 200" do
@@ -60,10 +241,14 @@ describe V1::LevelsController do
       expect(response.status).to eq(200)
     end
 
+    # --------------- #
+
     it "should return 1 record" do
       get :show, { 'id' => @level1.id }
       expect(JSON.parse(response.body)['data'].length).to eq(4) # 4 fields for one record (id, name, handicap, description,)
     end
+
+    # --------------- #
 
     it "should return the correct data for level1" do
       get :show, { 'id' => @level1.id }
@@ -73,6 +258,8 @@ describe V1::LevelsController do
       expect(JSON.parse(response.body)['data']['description'].to_s).to eq(@level1.description.to_s)
     end
 
+    # --------------- #
+
     it "should return the correct data for level2" do
       get :show, { 'id' => @level2.id }
       expect(JSON.parse(response.body)['data']['id'].to_i).to eq(@level2.id.to_i)
@@ -81,15 +268,21 @@ describe V1::LevelsController do
       expect(JSON.parse(response.body)['data']['description'].to_s).to eq(@level2.description.to_s)
     end
 
+    # --------------- #
+
     it "should return the correct error if the level id can't be found" do
+      #NOTE: + 2 comes from user model needing to create a level
       level4 = Level.last
-      get :show, { 'id' => level4.id + 1 }
-      expect(JSON.parse(response.body)['errors'].to_s).to eq("The level with id #{level4.id + 1} could not be found.")
+      get :show, { 'id' => level4.id + 2 }
+      expect(JSON.parse(response.body)['errors'].to_s).to eq("The level with id #{level4.id + 2} could not be found.")
     end
 
+    # --------------- #
+
     it "should return the correct status if the level id can't be found" do
+      #NOTE: + 2 comes from user model needing to create a level
       level4 = Level.last
-      get :show, { 'id' => level4.id + 1 }
+      get :show, { 'id' => level4.id + 2 }
       expect(response.status).to eq(422)
     end
   end
