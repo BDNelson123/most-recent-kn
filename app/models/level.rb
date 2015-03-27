dc77d2918e0d7d67dce7506e3aaada6c0f2807f4
BDNelson123 <ben.david.nelson@gmail.com>
2015-03-27 13:08:56 -0400

    just rearranged handicap validation to bottom of list

--- app/models/level.rb
+++ app/models/level.rb
@@ -2,8 +2,8 @@ class Level < ActiveRecord::Base
   belongs_to :user
   validates :name, :presence => true
   validates_uniqueness_of :name, :case_sensitive => false
-  validates :handicap, :presence => true
   validates :description, :presence => true
+  validates :handicap, :presence => true
  
   scope :common_attributes, -> { select('id, name, handicap, description')}
 end
