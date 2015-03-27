2646c145c0303f7cb3a2857f574cdae228c374a3
BDNelson123 <ben.david.nelson@gmail.com>
2015-03-27 12:31:46 -0400

    removed name from club factory in spec_helper to help with name unique validation

--- spec/modules/spec_helpers.rb
+++ spec/modules/spec_helpers.rb
@@ -13,7 +13,7 @@ module SpecHelpers
   def create_user
     @income = FactoryGirl.create(:income)
     @level = FactoryGirl.create(:level)
-    @club = FactoryGirl.create(:club, :name => "Wilson")
+    @club = FactoryGirl.create(:club)
     @user = FactoryGirl.create(:user, :wood_club_id => @club.id, :iron_club_id => @club.id, :level_id => @level.id, :income_id => @income.id)
   end
 end
