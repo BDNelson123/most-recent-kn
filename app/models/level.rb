95f4e22e121b3e072075d58594bd35d03d29d8e5
BDNelson123 <ben.david.nelson@gmail.com>
2015-04-03 12:30:08 -0400

    search attributes for levels and changed association with user

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,9 +1,19 @@
 class Level < ActiveRecord::Base
-  belongs_to :user
+  include ActiveRecordBaseExtensionScope
+
+  has_many :users 
+
   validates :name, :presence => true
   validates_uniqueness_of :name, :case_sensitive => false
   validates :description, :presence => true
   validates :handicap, :presence => true
  
   scope :common_attributes, -> { select('id, name, handicap, description')}
+
+  scope :search_attributes, -> (params) { 
+    if params[:search] != nil
+      return where("levels.name LIKE ? OR levels.handicap LIKE ? OR levels.description LIKE ?", "%#{params[:search]}%","%#{params[:search]}%","%#{params[:search]}%")
+    end
+    nil
+  }
 end
