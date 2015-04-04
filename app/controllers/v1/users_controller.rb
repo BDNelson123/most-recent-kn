89e15b090db6492cc8e2d4f2bc932dcb8aa3577c
BDNelson123 <ben.david.nelson@gmail.com>
2015-04-04 17:01:31 -0400

    working users index but only returns one feature

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -4,16 +4,18 @@ class V1::UsersController < ApplicationController
   before_action :authenticate_user!, :only => [:index, :show, :destroy], :unless => :master_api_key?
 
   def index
-    render :json => { :data => User.paginate(:page => params[:page], :per_page => params[:per_page]).common_attributes.all.order(params[:order_by] => params[:order_direction]) }, :status => 200
+    render :json => { :data => User.user_join.search_attributes(params).user_group(params).main_index(params) }, :status => 200
+  rescue ActiveRecord::StatementInvalid => error
+    render :json => { :errors => "Your query is invalid." }, :status => 422
   end
 
   def show
-    user = User.common_attributes.find_by_id(params[:id])
+    user = User.user_join.common_attributes.find_by_id(params[:id])
 
-    if user
-      render :json => { :data => user }, :status => 200
-    else
+    if user.id == nil
       render :json => { :errors => "The user with id #{params[:id]} could not be found." }, :status => 422
+    else
+      render :json => { :data => user }, :status => 200
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,18 @@
 class User < ActiveRecord::Base
+  include ActiveRecordBaseExtensionScope
+
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, 
          :validatable, :omniauthable, :confirmable
   include DeviseTokenAuth::Concerns::User
 
-  has_one :level
-  has_one :package
-  has_one :course
-  has_one :wood_club, class_name: "Club", foreign_key: :wood_club_id
-  has_one :iron_club, class_name: "Club", foreign_key: :iron_club_id
-  has_one :income
-  has_one :bay
+  belongs_to :level
+  belongs_to :package # NOTE: package is not required on create - reason for left join in user_join
+  belongs_to :course # NOTE: favorite course is not required on create - reason for left join in user_join
+  belongs_to :wood_club, class_name: "Club", foreign_key: :wood_club_id
+  belongs_to :iron_club, class_name: "Club", foreign_key: :iron_club_id
+  belongs_to :income
 
   validates :name, :presence => true
   validates :address, :presence => true
@@ -37,5 +38,48 @@ class User < ActiveRecord::Base
   validates :income_id, :presence => true
   validates_model_id :income_id, :message => "must be a valid income option", :model => Income
 
-  scope :common_attributes, -> { select('name, nickname, image, email, address, address2, city, state, zip, phone, dob, handedness, owns_clubs, email_optin, terms_accepted, gender')}
+  scope :common_attributes, -> { select('
+      users.id, users.name as user_name, users.nickname, users.image, users.email, users.address, users.address2, 
+      users.city, users.state, users.zip, users.phone, users.dob, users.handedness, users.owns_clubs, 
+      users.email_optin, users.terms_accepted, users.gender, users.level_id, levels.name as level_name,
+      iron_club.id as iron_club_id, iron_club.name as iron_club_name, wood_club.id as wood_club_id, 
+      wood_club.name as wood_club_name, users.course_id, courses.name as course_name, 
+      users.income_id, incomes.name as income_name, users.package_id, packages.name as packages_name,
+      GROUP_CONCAT(features.name) as package_features
+    ')
+  }
+
+  scope :user_join, -> { joins(:level,:income,"
+      JOIN clubs iron_club ON users.iron_club_id = iron_club.id 
+      JOIN clubs wood_club ON users.wood_club_id = wood_club.id 
+      LEFT JOIN courses ON users.course_id = courses.id
+      LEFT JOIN packages ON users.package_id = packages.id
+      LEFT JOIN featurizations ON users.package_id = featurizations.id 
+      LEFT JOIN features ON featurizations.feature_id = features.id
+    ")
+  }
+
+  scope :search_attributes, -> (params) { 
+    if params[:search] != nil
+      return where("
+        users.name LIKE ? OR users.address LIKE ? OR users.address2 LIKE ? OR
+        users.city LIKE ? OR users.state LIKE ? OR users.zip LIKE ? OR
+        users.phone LIKE ? OR users.dob LIKE ? OR users.email LIKE ? OR
+        iron_club.name LIKE ? OR packages.name LIKE ? OR features.name LIKE ? OR
+        users.address LIKE ?", 
+        "%#{params[:search]}%","%#{params[:search]}%","%#{params[:search]}%","%#{params[:search]}%",
+        "%#{params[:search]}%","%#{params[:search]}%","%#{params[:search]}%","%#{params[:search]}%",
+        "%#{params[:search]}%","%#{params[:search]}%","%#{params[:search]}%","%#{params[:search]}%",
+        "%#{params[:search]}%"
+      )
+    end
+    nil
+  }
+
+  scope :user_group, -> (params) { 
+    if params[:count] != "true"
+      return group("users.id")
+    end
+    nil
+  }
 end
