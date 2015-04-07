da7b4ed2cdde53dfd90ef580b9729726bae01862
BDNelson123 <ben.david.nelson@gmail.com>
2015-04-07 09:50:52 -0400

    added features to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
   namespace :v1 do
+    resources :assignments
+    resources :bays
     resources :clubs
     resources :courses
+    resources :features
+    resources :incomes
     resources :levels
     resources :packages
-    resources :bays
-    resources :assignments
     resources :users
-    resources :incomes
   end
 end
