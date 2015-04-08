76a59e04a70ad3b9d044139339223f4cb2609759
BDNelson123 <ben.david.nelson@gmail.com>
2015-04-08 12:31:01 -0400

    admin and employee models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
+  as :auth do
+  end
+
+  mount_devise_token_auth_for 'Admin', at: 'admin'
+  as :admin do
+  end
+
+  mount_devise_token_auth_for 'Employee', at: 'employee'
+  as :employee do
+  end
 
   namespace :v1 do
     resources :assignments
