752c921346661f04a74721bde0e98ae2b8e9b035
BDNelson123 <ben.david.nelson@gmail.com>
2015-04-09 15:48:01 -0400

    bays controller and model

--- app/controllers/v1/bays_controller.rb
+++ app/controllers/v1/bays_controller.rb
@@ -1,11 +1,64 @@
 class V1::BaysController < ApplicationController
   respond_to :json
+  before_filter :set_params, :only => [:index]
+  devise_token_auth_group :all, contains: [:user, :employee, :admin]
+  before_action :authenticate_admin!, :only => [:create, :destroy, :update], :unless => :master_api_key?
+  before_action -> { custom_authenticate_member(current_all) }, only: [:index, :show]
+
+  def create
+    bay = Bay.new(bay_params)
+
+    if bay.save
+      render :json => { :data => Bay.joins(:bay_status, :bay_type).common_attributes.find_by_id(bay.id) }, :status => 201
+    else
+      render :json => { :errors => bay.errors.full_messages.to_sentence }, :status => 422
+    end
+  end
+
+  def destroy
+    bay = Bay.find_by_id(params[:id])
+
+    if bay.blank?
+      render :json => { :errors => "The bay with id #{params[:id]} could not be found." }, :status => 422
+    else
+      bay.destroy
+      render :json => { :data => "The bay with id #{params[:id]} has been deleted." }, :status => 202
+    end
+  end
 
   def index
-    render :json => Bay.all
+    render :json => { :data => Bay.joins(:bay_status, :bay_type).search_attributes(params).main_index(params) }, :status => 200
+  rescue ActiveRecord::StatementInvalid => error
+    render :json => { :errors => "Your query is invalid." }, :status => 422
   end
 
   def show
-    render :json => Bay.find_by_id(params[:id])
+    bay = Bay.joins(:bay_status, :bay_type).common_attributes.find_by_id(params[:id])
+
+    if bay.id != nil
+      render :json => { :data => bay }, :status => 200
+    else
+      render :json => { :errors => "The bay with id #{params[:id]} could not be found." }, :status => 422
+    end
+  end
+
+  def update
+    bay = Bay.find_by_id(params[:id])
+
+    if bay.blank?
+      render :json => { :errors => "The bay with id #{params[:id]} could not be found." }, :status => 422
+    elsif bay.update(bay_params)
+      render :json => { :data => Bay.joins(:features).common_attributes.find_by_id(bay.id) }, :status => 200
+    else
+      render :json => { :errors => bay.errors.full_messages.to_sentence }, :status => 422
+    end
+  end
+
+  private
+
+  def bay_params
+    _params = params.require(:bay).permit(
+      :name, :description, :price, :credits, :featurizations_attributes => [:feature_id]
+    )
   end
 end
--- app/models/bay.rb
+++ app/models/bay.rb
@@ -1,4 +1,19 @@
 class Bay < ActiveRecord::Base
+  include ActiveRecordBaseExtensionScope
+
   belongs_to :bay_type
   belongs_to :bay_status
+
+  validates :number, :presence => true
+  validates :bay_type_id, :presence => true
+  validates :bay_status_id, :presence => true
+
+  scope :common_attributes, -> { select('bays.id, bays.number, bay_statuses.name as status_name, bay_statuses.description as status_description, bay_types.name as type_name, bay_types.description as type_description, bay_types.credits_per_hour as type_credit')}
+
+  scope :search_attributes, -> (params) { 
+    if params[:search] != nil
+      return where("bays.number LIKE ? OR bay_statuses.name LIKE ? OR bays_statuses.description LIKE ? OR bay_types.name LIKE ? OR bay_types.description LIKE ? OR bay_types.credits_per_hour LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
+    end
+    nil
+  }
 end
