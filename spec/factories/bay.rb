33f8b05dd733750acb2af56a7a19eb8ce6f4a046
BDNelson123 <ben.david.nelson@gmail.com>
2015-04-09 16:03:46 -0400

    bay factory

---
+++ spec/factories/bay.rb
@@ -0,0 +1,10 @@
+require 'factory_girl'
+require 'faker'
+
+FactoryGirl.define do
+  factory :bay do
+    number { Faker::Address.building_number }
+    bay_type_id { Faker::Number.number(2) }
+    bay_status_id { Faker::Number.number(2) }
+  end
+end
